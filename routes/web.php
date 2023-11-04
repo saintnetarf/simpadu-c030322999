762c4b7e41b22a10ae891286f3cefba62c35db05
saintnetarf <saintnetarf@gmail.com>
2023-11-04 22:24:35 +0800

    login fortify

--- routes/web.php
+++ routes/web.php
@@ -14,28 +14,42 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('pages.auth.auth-login', ['type_menu' => '']);
+});
 
-      return view('pages.app.dashboard-simpadu', ['type_menu'=> '']);
+Route::middleware(['auth'])->group(function () {
 
-  });
+    Route::get('home', function () {
+        return view('pages.app.dashboard-simpadu', ['type_menu' => '']);
+    })->name('home');
+});
 
-Route::get('/login', function () {
 
-    return view('pages.auth.auth-login');
 
-})->name('login');
-Route::get('/register', function () {
 
-      return view('pages.auth.auth-register');
+// Route::get('/', function () {
 
-})->name('register');
-Route::get('/forgot', function () {
+//       return view('pages.app.dashboard-simpadu', ['type_menu'=> '']);
 
-    return view('pages.auth.auth-forgot-password');
+//   });
 
-})->name('forgot');
-Route::get('/reset', function () {
+// Route::get('/login', function () {
 
-    return view('pages.auth.auth-reset-password');
+//     return view('pages.auth.auth-login');
 
-})->name('reset');
+// })->name('login');
+// Route::get('/register', function () {
+
+//       return view('pages.auth.auth-register');
+
+// })->name('register');
+// Route::get('/forgot', function () {
+
+//     return view('pages.auth.auth-forgot-password');
+
+// })->name('forgot');
+// Route::get('/reset', function () {
+
+//     return view('pages.auth.auth-reset-password');
+
+// })->name('reset');
